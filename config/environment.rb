8779be16264733383d174ba27855c53f947b9502
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2010-02-25 18:33:43 +0000

    require treetop

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  
+  config.gem "treetop"
   config.gem "andand"
   config.gem "stemmer"
   #config.gem "twitter"
