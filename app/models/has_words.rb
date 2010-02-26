7a9e7658b74f96818a246e0054717369ceaf3000
Chris O'Sullivan <thechrisoshow@gmail.com>
2010-02-26 11:06:25 +0000

    Fixed word count

--- app/models/has_words.rb
+++ app/models/has_words.rb
@@ -6,12 +6,17 @@ module HasWords
   
   def word_counts
     raise "The record must be saved!" if self.new_record?
-    connection.execute("
+    result = connection.execute("
       SELECT count(#{words_join_table_name}.id)
       FROM words 
       LEFT JOIN #{words_join_table_name} ON #{words_join_table_name}.word_id = words.id and #{words_join_table_name}.#{self.class.table_name.singularize}_id = #{self.id}
       GROUP BY words.id
-      ORDER BY words.id;").inject([]) {|m, x| m +  [x.first.to_i]}
+      ORDER BY words.id;").map {|h| h.values.last.to_i }
+      # [{0=>"1", "count(word_uses.id)"=>"1"}, {0=>"2", "count(word_uses.id)"=>"2"}, {0=>"3", "count(word_uses.id)"=>"3"}]  
+      
+      
+      
+      #.inject([]) {|m, x| m +  [x.first.to_i]}
   end
 end
   
