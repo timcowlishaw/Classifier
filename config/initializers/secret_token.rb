64a96a6b7e18a8c3d5da87d79b572bba1c054d1f
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2010-07-02 16:00:54 +0100

    new RAILS 3 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'c3ce5f551b126170a5e37e186bc24ff34732fb22b54d83ded3bf8957a65d3c44238c6721620aca89dcc0a4a6fc1099fd5e71bd8c79c6e29db50bf8d967d4d0ed'
